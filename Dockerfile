4f6e03fed77fc710bd89c25bda167262786e8704
Tobias Florek <tob@butter.sh>
2016-09-26 19:49:37 +0200

    use openshift/base-centos7 as base image

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,10 @@
-FROM alpine:3.2
+FROM openshift/base-centos7
 MAINTAINER tob@butter.sh
 
 # Install system utils & Gogs runtime dependencies
-RUN echo "@edge http://dl-4.alpinelinux.org/alpine/edge/main" | tee -a /etc/apk/repositories \
- && echo "@testing http://dl-4.alpinelinux.org/alpine/edge/testing" | tee -a /etc/apk/repositories \
- && apk -U --no-progress upgrade \
- && apk -U --no-progress add autossh@testing \
- && mkdir /conf \
- && chmod 0777 /conf \
- && adduser autossh -Du 1000 -h /home/autossh \
- && mkdir -p /home/autossh/.ssh \
- && chmod 0700 /home/autossh/.ssh \
- && chown -R autossh /home/autossh
+RUN yum install -y --setopt=tsflags=nodocs autossh nss_wrapper \
+ && mkdir -p /conf /opt/app-root/etc /opt/app-root/src \
+ && chmod 0777 /conf /opt/app-root/etc /opt/app-root/src
 
 ADD start.sh /
 
--- start.sh
+++ start.sh
@@ -1,18 +1,41 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+set -euo pipefail
 
-USER=$(whoami)
-export USER
+generate_container_user() {
+    local USER_ID; USER_ID="$(id -u)"
+    local GROUP_ID; GROUP_ID="$(id -g)"
+
+    if [ x"$USER_ID" != x"0" -a x"$USER_ID" != x"1001" ]; then
+
+        NSS_WRAPPER_PASSWD=/opt/app-root/etc/passwd
+        NSS_WRAPPER_GROUP=/etc/group
+
+        sed -e 's/^default:/builder:/' < /etc/passwd > $NSS_WRAPPER_PASSWD
+
+        echo "default:x:${USER_ID}:${GROUP_ID}:Default Application User:${HOME}:/sbin/nologin" >> $NSS_WRAPPER_PASSWD
+
+        export NSS_WRAPPER_PASSWD
+        export NSS_WRAPPER_GROUP
+
+        LD_PRELOAD=libnss_wrapper.so
+        export LD_PRELOAD
+    fi
+}
+
+generate_container_user
+
+export USER="$(whoami)"
+
+mkdir -p "$HOME/.ssh"
+chown 0700 "$HOME/.ssh" || true
 
 if [ -f /secrets/known-hosts ]; then
     cp /secrets/known-hosts $HOME/.ssh/known_hosts
 fi
 
-AUTOSSH_DEBUG=true
-export AUTOSSH_DEBUG
+export AUTOSSH_DEBUG="${AUTOSSH_DEBUG-true}"
 
-AUTOSSH_LOGFILE=$HOME/log
-export AUTOSSH_LOGFILE
+export AUTOSSH_LOGFILE=$HOME/log
 mkfifo $AUTOSSH_LOGFILE
 
 # log to stdout
